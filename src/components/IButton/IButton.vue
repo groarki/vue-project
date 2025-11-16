222a224a8f47be3fa77bea59cc4f82aab935bce5
Yelyzaveta Shevchenko <lizkasheva26@gmail.com>
2025-11-16 20:46:24 +0100

    fix favicon, home page sizes and button

--- src/components/IButton/IButton.vue
+++ src/components/IButton/IButton.vue
@@ -1,6 +1,6 @@
 <template>
   <button
-    class="bg-[#FFA279] transition delay-150 duration-150 ease-out hover:shadow-xl hover:scale-95 rounded-xl py-2.5 px-6 text-white cursor-pointer text-sm font-bold tracking-tighter"
+    class="bg-[#FFA279] transition delay-150 duration-150 ease-out hover:shadow-xl hover:scale-95 rounded-xl py-3 px-7 text-white cursor-pointer text-xs font-bold"
   >
     Почати роботу
   </button>
