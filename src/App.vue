91256ec5470f9ee1b11c691a4a7164d150f01be8
Yelyzaveta Shevchenko <lizkasheva26@gmail.com>
2025-11-21 20:38:44 +0100

    form actions

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
+import LoginForm from './components/Auth/LoginForm/LoginForm.vue'
 import RegistrationForm from './components/Auth/Registrationform/RegistrationForm.vue'
 import HomePage from './views/HomePage.vue'
 </script>
 
 <template>
   <HomePage />
-  <RegistrationForm />
+  <RegistrationForm @submit="console.log" />
+  <LoginForm @submit="console.log" />
 </template>
---
+++ src/components/Auth/FormContainer/FormContainer.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+
+<template>
+  <form class="flex flex-col p-10 w-[499px] [&>*:not(:nth-child(3))]:mb-4">
+    <slot></slot>
+  </form>
+</template>
